8ce8f6b9753988ddda431614ee954b1eb8b4d14b
pedram-pema <studentpedramp@gmail.com>
2024-07-10 17:44:11 +0330

    change .boards element from div to p for semantic HTML
    Clean script file by deleting comments

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 let playerScore = 0;
 let computerScore = 0;
-/* variables above are declared to be used for counting score at the end of match and each round.
-Each win is counted as one point for the winner.
- */
 
 let rock = document.querySelector(".rock");
 let paper = document.querySelector(".paper");
@@ -93,19 +90,6 @@ scissors.addEventListener('click', () => {
 function playSingleRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
         console.log('Round tied! You have to choose a weapon again.');
-/*         playerSelection = getPlayerChoice();
-        if (playerSelection == undefined) { //could return null
-            let cancelGameRequest = confirm('Do you want to cancel the game?');
-            if (cancelGameRequest) {
-                console.log('You have canceled the game.')
-                return; 
-            } else {
-                console.log('OK! Continue the game.');  
-                playerSelection = getPlayerChoice();
-            }
-        }
-        computerSelection = getComputerChoice();
-        return playSingleRound(playerSelection, computerSelection); */
     } else if (playerSelection === 'Rock' 
                 && computerSelection === 'Scissors') {
         playerScore++;
@@ -134,28 +118,3 @@ function playSingleRound(playerSelection, computerSelection) {
         console.log('No second chance this time');
     }
 }
-
-/* function showCommentary() {
-    if (playerScore > computerScore 
-        && playerScore !== 3 
-        && computerScore !== 3) {
-        return `You are ahead ${playerScore}-${computerScore}! Keep it up.`;
-    } else if (computerScore > playerScore 
-                && playerScore !== 3 
-                && computerScore !== 3) {
-        return `You are loosing ${playerScore}-${computerScore}! Do something.`;
-    } else if (playerScore === computerScore 
-                && playerScore !== 3 
-                && computerScore !== 3) {
-        return `Score is tied ${playerScore}-${computerScore}! Hurry up.`;
-    } else if (playerScore === 3 
-                || computerScore === 3) {
-        return `Final result is: ${playerScore}-${computerScore}!`;
-    }
-}
- */
-// function replay() {
-//     game();
-// }
-
-// game must react differently to each score
